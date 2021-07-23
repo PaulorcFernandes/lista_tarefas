647433edc74fc2b75307737a46b9423b84c03ec8
Paulo Fernandes <paulorcfer@gmaill>
2021-07-23 15:35:01 -0300

    adicionado refresh

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,19 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b){
+        if(a["ok"] && !b["ok"]) return 1;
+        else if(!a["ok"] && b["ok"]) return 1;
+        else return 0;
+      });
+      _saveData();
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,10 +93,11 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
+              child: RefreshIndicator(onRefresh: _refresh,
               child: ListView.builder(
                   padding: EdgeInsets.only(top: 10.0),
                   itemCount: _toDoList.length,
-                  itemBuilder: buildItem),
+                  itemBuilder: buildItem),)
               )
         ],
       ),
@@ -141,6 +155,7 @@ class _HomeState extends State<Home> {
             ),
             duration: Duration(seconds: 3),
           );
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
       },
